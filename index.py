6e2ab766bc28c8ea68c3f20989a1d2e8023f7687
OrgesaD <orgesadibrani7@gmail.com>
2024-07-10 02:54:55 -0700

    versioni 1

---
+++ index.py
@@ -0,0 +1,12 @@
+x = int(input("shkruaje numrin e pikeve:"))
+if ((x >= 0) and (x <= 29)):
+    print("ti ke ngel e ki noten 1")
+elif((x >= 30) and (x <= 49)):
+    print("ti e ki noten 2")
+elif((x >= 50) and (x <= 69)):
+    print("ti e ki noten 3")
+elif((x >= 70) and (x <= 89)):
+    print("ti e ki noten 4")
+elif((x >= 90) and (x <= 100)):
+    print("ti e ki noten 5")
+    
